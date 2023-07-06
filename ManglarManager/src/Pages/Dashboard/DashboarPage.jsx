2c3eb3ff9dda59d8cd15d3ff23f6834059c5d2bd
ctomas2021215 <ctomas-2021215@kinal.edu.gt>
2023-07-06 15:50:26 -0600

    se subio base proyecto

---
+++ ManglarManager/src/Pages/Dashboard/DashboarPage.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { AuthContext } from '../..'
+//import Swal from 'sweetalert2'
+import { Outlet, Link } from 'react-router-dom'
+
+export const  DashboarPage = () => {
+    return(
+        <>
+        <div>DASHBOARD</div>
+        </>
+    )
+}
---
+++ ManglarManager/src/index.jsx
@@ -0,0 +1,66 @@
+//import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+//import "@fortawesome/fontawesome-free/css/all.min.css";
+import React, { useState, createContext, useEffect} from "react";
+import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { DashboarPage } from "./Pages/Dashboard/DashboarPage";
+import {NotFound} from "./Pages/NotFound"
+import { LoginPage } from "./LoginPage";
+
+
+export const AuthContext = createContext();
+
+export const Index = () => {
+    const [loggedIn, setLoggedIn] = useState(false)
+    const [dataUser, setDataUser] = useState({
+
+        name: '',
+        surname: '',
+        username: '',
+        phone: '',
+        email: '',
+        role:'',
+        
+    })
+
+        useEffect(()=> {
+            let token = localStorage.getItem('token')
+            if(token) setLoggedIn(true)
+
+
+            //mantener sesion
+            let user = JSON.parse(localStorage.getItem('lario'))
+                if(user){
+                    setDataUser(user)
+                }
+        }, [])
+
+        const routes = createBrowserRouter([
+            {
+                path: '/',
+                element: <App/>,
+                errorElement: <NotFound/>,
+
+                children:[
+                    {
+                        path:'/login',
+                        element: <LoginPage></LoginPage>
+                    },
+                    {
+                        path:'/panel',
+                        element: loggedIn ? <DashboarPage/> : <LoginPage/>,
+                        children: [
+                           
+                           ]
+                    }
+                ]
+
+
+        }])
+
+        return (
+            <AuthContext.Provider value={{loggedIn,setLoggedIn,dataUser,setDataUser}}>
+                <RouterProvider router={routes}/>
+            </AuthContext.Provider>
+          )
+        }
